da02269834f1f944918c0ebc7d7f8829c3c048ef
Arturas <a.molcanovas@gmail.com>
2021-05-06 23:46:31 +0100

    feat: Add exposeWasm option

--- projects/core/autoinit-wasm-import.js
+++ projects/core/autoinit-wasm-import.js
@@ -1,11 +1,24 @@
-export default function autoinitWasmImport(imp, url) {
-  return Promise
-    .all([
-      imp,
-      fetch(url)
-    ])
-    .then(function wasmImportInitialiser([wasmModule, response]) {
-      return wasmModule.default(response)
-        .then(() => wasmModule);
-    });
+function createGetter(initResponse) {
+  const getter = {};
+  Object.defineProperty(getter, '__getWasm', {
+    value: function __getWasm() {
+      return initResponse;
+    }
+  });
+
+  return getter;
+}
+
+/**
+ * @param {Promise<any>} imp - The dynamic import
+ * @param {string} url - The wasm file's URL
+ * @param {boolean} exposeWasm - Whether to expose the init function's response or not
+ */
+export default async function autoinitWasmImport(imp, url, exposeWasm) {
+  const [wasmModule, fetchResponse] = await Promise.all([imp, fetch(url)]);
+  const initResponse = await wasmModule.default(fetchResponse);
+
+  return exposeWasm ?
+    Object.assign(createGetter(initResponse), wasmModule) :
+    wasmModule;
 }
--- projects/core/index.spec.ts
+++ projects/core/index.spec.ts
@@ -1,34 +1,73 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import {expect} from 'chai';
 import {join} from 'path';
-import type {OutputChunk, RollupOutput} from 'rollup';
+import type {OutputChunk, RollupBuild, RollupOutput} from 'rollup';
 import {rollup} from 'rollup';
+import type {Opts} from './index';
 import {wasmBindgen} from './index';
 
-describe('PlaceholderTestSuite', function () {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const alias = require('@rollup/plugin-alias');
+
+function makeBuild(opts?: Partial<Opts>): Promise<RollupBuild> {
+  return rollup({
+    input: join(__dirname, 'test-input.js'),
+    plugins: [
+      nodeResolve(),
+      alias({
+        entries: {
+          '@alorel/rollup-plugin-wasm-bindgen-web/autoinit-wasm-import': join(__dirname, 'autoinit-wasm-import.js')
+        }
+      }),
+      wasmBindgen({
+        ...opts,
+        wasmFilter: {
+          include: /core[\\/]fixture-output[\\/]index\.js$/,
+          ...opts?.wasmFilter
+        }
+      })
+    ]
+  });
+}
+
+function makeBundle(build: RollupBuild): Promise<RollupOutput> {
+  return build.generate({
+    format: 'es',
+    preferConst: true,
+    preserveModules: true
+  });
+}
+
+function makeRegex(exposeWasm: boolean): RegExp {
+  return new RegExp(`(autoinitWasmImport|__wasmBindgenRollupPluginDynamicImportLoader)\\(import\\(['"].+['"]\\), .+, ${exposeWasm}\\)`);
+}
+
+function findEntryChunk(bundle: RollupOutput): OutputChunk | undefined {
+  return bundle.output.find((f): f is OutputChunk => f.type === 'chunk' && f.isEntry)!;
+}
+
+describe('With wasm exposed', () => {
+  let bundle: RollupOutput;
+  let entryChunk: OutputChunk | undefined;
+  before(async () => {
+    bundle = await makeBundle(await makeBuild({exposeWasm: true}));
+    entryChunk = findEntryChunk(bundle);
+  });
+
+  it('Should call init factory with true as the exposeWasm argument', () => {
+    expect(entryChunk).to.not.eq(undefined, 'entry chunk not found');
+    expect(entryChunk!.code).to.match(makeRegex(true));
+  });
+});
+
+describe('Without wasm exposed', function () {
   let bundle: RollupOutput;
   let entryChunk: OutputChunk | undefined;
 
   before('build', async () => {
-    const build = await rollup({
-      input: join(__dirname, 'test-input.js'),
-      plugins: [
-        nodeResolve(),
-        wasmBindgen({
-          wasmFilter: {
-            include: /core[\\/]fixture-output[\\/]index\.js$/
-          }
-        })
-      ]
-    });
-
-    bundle = await build.generate({
-      format: 'es',
-      preferConst: true,
-      preserveModules: false
-    });
-
-    entryChunk = bundle.output.find((f): f is OutputChunk => f.type === 'chunk' && f.isEntry)!;
+    const build = await makeBuild();
+    bundle = await makeBundle(build);
+    entryChunk = findEntryChunk(bundle);
   });
 
   it('Should have a wasm asset', () => {
@@ -36,9 +75,9 @@ describe('PlaceholderTestSuite', function () {
   });
 
   it('Should render entry chunk with Promise.all', () => {
-    expect(entryChunk).to.not.eq(undefined, 'undefined');
+    expect(entryChunk).to.not.eq(undefined, 'entry chunk not found');
     const c = entryChunk!.code;
-    expect(c).to.include('import __wasmBindgenRollupPluginDynamicImportLoader from \'@alorel/rollup-plugin-wasm-bindgen-web/autoinit-wasm-import\';');
-    expect(c).to.match(/__wasmBindgenRollupPluginDynamicImportLoader\(\nimport\(['"].+['"]\),\n.+\n\)/);
+    expect(c).to.match(/import (autoinitWasmImport|__wasmBindgenRollupPluginDynamicImportLoader) from ['"].+autoinit-wasm-import.*['"];\n/);
+    expect(c).to.match(makeRegex(false));
   });
 });
